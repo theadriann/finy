0c3063c623359ea3bc13fab727bc0843f11e9210
theadriann <adrian.brojbeanu@gmail.com>
2020-09-23 01:40:32 +0300

    updating manifest

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,12 @@ import * as React from "react";
 import { render } from "react-dom";
 import "mobx-react/batchingForReactDom";
 
+// app
 import App from "./App";
+
+// persistent storage
+if (navigator.storage && navigator.storage.persist) {
+    navigator.storage.persist();
+}
 
 render(<App />, document.getElementById("root"));
